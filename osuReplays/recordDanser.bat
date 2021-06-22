2bc9827ad3ad991933116dbf3be8a6850bbb4f0e
robbyishere <jkoh1295@gmail.com>
2021-06-21 22:10:46 -0700

    Fixed recordDanser.bat

--- osuReplays/recordDanser.bat
+++ osuReplays/recordDanser.bat
@@ -1,2 +1,3 @@
+SET REPLAYFOLDER="C:\Users\PC\Documents\osu!\Replays
 SET /P REPLAY="[Enter Replay Filename] - "
-C:\Users\PC\Documents\GitHub\Projects\osuReplays\danser-0.5.2-win\danser -r %REPLAY% -skin "Machiron Mix-skin Remastered 1.8"
+.\danser-0.5.2-win\danser -r=%REPLAYFOLDER%\%REPLAY%" -skin "Machiron Mix-skin Remastered 1.8" -record
